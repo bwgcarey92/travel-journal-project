6adbc6626c3ecd5535f9b0f9cbc101993b7fad0d
bwgcarey92 <148415061+bwgcarey92@users.noreply.github.com>
2024-06-07 23:51:53 -0400

    added styling for card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,12 +7,14 @@ export default function Card(){
         <div className='card-container'>
             <img src={mtFuji} className='card-img' />
             <div className='card-info'>
-                <img src={location} className='location-icon' />
-                <p className='destination'>Japan</p>
-                <a className='googlemaps-anchor'>View on Google Maps</a>
+                <div className='location-info'>
+                    <img src={location} className='location-icon' />
+                    <p className='destination'>Japan</p>
+                    <a href='#' className='googlemaps-anchor'>View on Google Maps</a>
+                </div>
                 <h2 className='country'>Mount Fuji</h2>
-                <p className='date-arrived'>12 Jan, 2021</p> 
-                <p className='date-depart'>- 24 Jan, 2021</p>
+                <div></div>
+                <p className='date'>12 Jan, 2021 - 24 Jan, 2021</p>
                 <p className='destination-descr'>Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
             </div>
         </div>
